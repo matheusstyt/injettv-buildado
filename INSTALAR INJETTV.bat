81dab1a54cb4c38ac3a50fe9a4b34ee7ea740680
matheusstyt <matheus20galdino@gmail.com>
2023-01-09 17:02:16 -0400

    new version

--- INSTALAR INJETTV.bat
+++ INSTALAR INJETTV.bat
@@ -1,3 +1,3 @@
 cd C:\Users\mathe\Documents\desenvolvimento\injettv-buildado
-buildado
+
 node install_services.js
--- install_services.js
+++ install_services.js
@@ -2,9 +2,9 @@ var Service = require('node-windows').Service;
 // Criando um novo objeto do Serviço
 var svc = new Service({
 //Nome do servico
-name:'InjetTv-Comentais',
+name:'InjetTv 2.0',
 //Descricao que vai aparecer no Gerenciamento de serviço do Windows
-description: 'InjetTV - Comentais',
+description: 'InjetTV - Dashboard IDW',
 //caminho absoluto do seu script
 script: 'C:\\Users\\mathe\\Documents\\desenvolvimento\\injettv-buildado\\www',
 scriptOptions: 'start'
